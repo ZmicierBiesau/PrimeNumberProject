491c41b468acdc202ea7c830a71f10674d9c118b
Zmicier Biesau <zmicier.biesau@gmail.com>
2015-06-29 04:06:38 +0400

    Adding core data

---
+++ PrimeNumber/HistoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  HistoryTableViewController.h
+//  PrimeNumber
+//
+//  Created by Maxvale on 29.06.15.
+//  Copyright (c) 2015 e.g. Arya Stark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HistoryTableViewController : UITableViewController
+
+@end
--- PrimeNumber/PrimeNumberProvider.m
+++ PrimeNumber/PrimeNumberProvider.m
@@ -26,7 +26,7 @@
     dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
         
         
-        NSInteger startNumber = 2;
+        NSInteger startNumber = 1;
         NSMutableArray *array = [[GeneratedData sharedGeneratedData] getCachedGeneratedArray];
         if (!array) {
             array = [NSMutableArray new];
@@ -43,7 +43,7 @@
         }
         else
         {
-            for (NSInteger i = startNumber; i <= limit; i++) {
+            for (NSInteger i = ++startNumber; i <= limit; i++) {
                 if ([self checkIfNumberPrime:i]) {
                     [array addObject:[NSNumber numberWithInteger:i]];
                 }
@@ -55,8 +55,9 @@
                 [_delegate primeNumberProvider:self didGenerateArray:[array copy]];
             });
         }
+        //NSLog(@"%@", array);
     });
-    //NSLog(@"%@", array);
+    
 }
 
 
@@ -65,6 +66,16 @@
 {
     for (NSInteger i = 2; i <= sqrt(number); i++) {
         if (number % i == 0) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+- (BOOL) checkIfNumberPrime: (NSInteger) number withHelpArray: (NSArray*) helpArray //unfortunatelly reading from array and transformin NSNumber to NSInteger is slower that just go through all suitable NSInteger numbers. I think reason of storaging NSInteger on stack and object type NSNUmber on heap. 
+{
+    for (NSInteger i = 0; i < [helpArray count] && [helpArray[i] integerValue] <= sqrt(number); i++) {
+        if (number % [helpArray[i] integerValue] == 0) {
             return NO;
         }
     }
